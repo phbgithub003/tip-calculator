ca916b184869105482349e849ecd089eae366b75
harshitp <palacheralaharshit@gmail.com>
2024-02-17 11:50:58 +0530

    initial commit

---
+++ Tip-calculator/Tip-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-calculator
+//
+//  Created by Harshit Bhargava  on 17/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
